023261e190bba5dbc81ad8349890e3b54c523e4f
Stephen Touset <stephen@touset.org>
2013-03-16 19:38:03 -0700

    Simplify Delegate greatly

--- lib/sodium/delegate.rb
+++ lib/sodium/delegate.rb
@@ -2,53 +2,52 @@ require 'sodium'
 
 module Sodium::Delegate
   def self.included(base)
+    base.send :extend, ClassMethods
     base.send :extend, self.class_methods(base)
   end
 
+  module ClassMethods
+    def inherited(base)
+      @_nacl_implementations ||= []
+      @_nacl_implementations <<  base
+
+      class << base
+        undef_method  :implementation=
+        define_method(:implementation) { self }
+      end
+    end
+
+    def primitive
+      self.implementation[:PRIMITIVE]
+    end
+
+    def implementation=(implementation)
+      @_nacl_default = implementation
+    end
+
+    private
+
+    def _find_implementation(name)
+      @_nacl_implementations.detect {|i| i.primitive == name }
+    end
+  end
 
   def self.class_methods(base)
     Module.new do
-      def inherited(base)
-        @_nacl_implementations ||= []
-        @_nacl_implementations <<  base
-
-        (class << base; self; end).send :undef_method, :implementation=
-      end
-
-      define_method :primitive do
-        self.implementation[:PRIMITIVE]
-      end
-
-      define_method :implementation= do |implementation|
-        @_nacl_default = implementation
-      end
-
       define_method :implementation do |*args|
         # seriously, fuck Ruby 1.8
         raise ArgumentError, "wrong number of arguments (#{args.length} for 0..1)" if
           args.length > 1
 
-        raise ArgumentError, "wrong number of arguments (#{args.length}) for 0)"   if
-          args.length != 0 and self != base
-
-        case
-          when self != base then self
-          when args.empty?  then @_nacl_default ||= _implementation(self::DEFAULT)
-          else                                      _implementation(args.first)
-        end
-      end
-
-      define_method :_implementation do |name|
-        @_nacl_implementations.detect {|i| i.primitive == name }
+        args.any? ?          _find_implementation(args.first) :
+          @_nacl_default ||= _find_implementation(self::DEFAULT)
       end
 
       define_method :new do |*args, &block|
-        self == base                             ?
-          self.implementation.new(*args, &block) :
-          super(*args, &block)
-      end
+        return super(*args, &block) unless self == base
 
-      private :_implementation
+        self.implementation.new(*args, &block)
+      end
     end
   end
 
