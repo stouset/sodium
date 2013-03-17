49cc206426e3eb8506fbc568a574fbb64582a9f3
Stephen Touset <stephen@touset.org>
2013-03-16 17:52:52 -0700

    Fix SimpleCov configuration for coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,17 @@
+require 'coveralls'
+require 'simplecov'
+
+SimpleCov.adapters.define 'sodium' do
+  add_filter '/test/'
+  add_filter '/vendor/'
+end
+
+Coveralls.wear!('sodium')
+
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'minitest/pride'
 
-require 'base64'
-
-require 'coveralls'
-Coveralls.wear!
-
 require 'sodium'
+
+require 'base64'
