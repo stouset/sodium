7270e2ebae9073181cb46f7c6eb78fd081e72fc1
Stephen Touset <stephen@touset.org>
2013-03-16 19:37:57 -0700

    Ensure DelegateTest class meets expectations

--- test/sodium/delegate_test.rb
+++ test/sodium/delegate_test.rb
@@ -3,6 +3,10 @@ require 'test_helper'
 class DelegateTest
   include Sodium::Delegate
 
+  def self.[](key)
+    self.const_get(key)
+  end
+
   class Subclass1 < self; PRIMITIVE = :subclass1; end
   class Subclass2 < self; PRIMITIVE = :subclass2; end
 
