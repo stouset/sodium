3e35788aec8781be93157d76479669852060f7cc
Stephen Touset <stephen@touset.org>
2013-05-22 16:45:40 -0700

    Rename variables to increase consistency
    The superclass that encodes the logic for a specific family of methods
    in the NaCl bindings is referred to as a delegate in other areas, so
    that name is now used consistently when it is stored in a
    variable. Likewise, subclasses that inherit from the delegate are also
    consistently assigned to appropriately-named variables.

--- lib/sodium/na_cl.rb
+++ lib/sodium/na_cl.rb
@@ -25,76 +25,76 @@ module Sodium::NaCl
     (class << klass; self; end)
   end
 
-  def self._install_default(scope, configuration)
+  def self._install_default(delegate, configuration)
     family    = configuration[:family]
-    method    = _install_function scope, family, nil, :PRIMITIVE, [ :string ]
-    primitive = scope.send(method)
+    method    = _install_function delegate, family, nil, :PRIMITIVE, [ :string ]
+    primitive = delegate.send(method)
   rescue FFI::NotFoundError
     primitive = configuration[:primitives].first
   ensure
-    scope.const_set :DEFAULT, primitive.downcase.to_sym
+    delegate.const_set :DEFAULT, primitive.downcase.to_sym
   end
 
-  def self._install_primitives(scope, configuration)
+  def self._install_primitives(delegate, configuration)
     configuration[:primitives].each do |primitive|
-      klass = Class.new(scope) do
+      subclass = Class.new(delegate) do
         def self.[](name)
           self.const_get(name)
         end
       end
 
-      scope.const_set primitive, klass
+      delegate.const_set primitive, subclass
 
-      _install_constants klass, configuration[:family], primitive,
+      _install_constants subclass, configuration[:family], primitive,
         configuration[:constants]
 
-      _install_functions klass, configuration[:family], primitive,
+      _install_functions subclass, configuration[:family], primitive,
         configuration[:functions]
     end
   end
 
-  def self._install_constants(klass, family, primitive, constants)
+  def self._install_constants(subclass, family, primitive, constants)
     constants = constants.each_with_object(
       :PRIMITIVE => :string
     ) {|constant, hash| hash[constant] = :size_t }
 
     constants.each_pair do |name, type|
-      _install_constant(klass, family, primitive, name, type)
+      _install_constant(subclass, family, primitive, name, type)
     end
   end
 
-  def self._install_constant(klass, family, primitive, name, type)
-    method = _install_function klass, family, primitive, name, [ type ]
+  def self._install_constant(subclass, family, primitive, name, type)
+    method = _install_function subclass, family, primitive, name, [ type ]
 
     family = family.to_s.upcase
     name   = name.to_s.upcase
-    value  = klass.send(method)
+    value  = subclass.send(method)
 
-    self. const_set("#{family}_#{primitive}_#{name}", value)
-    klass.const_set(name,                             value)
+    self.    const_set("#{family}_#{primitive}_#{name}", value)
+    subclass.const_set(name,                             value)
   end
 
-  def self._install_functions(klass, family, primitive, methods)
+  def self._install_functions(subclass, family, primitive, methods)
     methods.each do |name, arguments|
-      _install_function(klass, family, primitive, name, arguments, &:zero?)
+      _install_function(subclass, family, primitive, name, arguments, &:zero?)
     end
   end
 
-  def self._install_function(klass, family, primitive, name, arguments, &converter)
+  def self._install_function(subclass, family, primitive, name, arguments, &converter)
     imp       = [ family, primitive, name ].compact.join('_').downcase
     meth      = [ 'nacl',            name ].compact.join('_').downcase
     arguments = arguments.map(&:to_sym)
 
     self.attach_function imp, arguments[0..-2], arguments.last
-    self.attach_method   imp, self, klass, meth, &converter
+    self.attach_method   imp, self, subclass, meth, &converter
 
     meth
   end
 
   CONFIG.each do |configuration|
-    scope = self._load_class configuration[:class]
+    delegate = self._load_class configuration[:class]
 
-    self._install_default    scope, configuration
-    self._install_primitives scope, configuration
+    self._install_default    delegate, configuration
+    self._install_primitives delegate, configuration
   end
 end
