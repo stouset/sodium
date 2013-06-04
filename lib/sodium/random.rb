c2f60d8fee5ac293f5655b724e236a14ff022461
Stephen Touset <stephen@touset.org>
2013-06-03 19:02:31 -0700

    Remove unnecessary mutex checking
    The randombytes API in libsodium is *only* non-threadsafe if
    `sodium_init` hasn't been called prior. Since we explicitly call it when
    the library is loaded, this is not actually a concern.

--- lib/sodium/random.rb
+++ lib/sodium/random.rb
@@ -1,26 +1,16 @@
 require 'sodium'
 
 module Sodium::Random
-  @mutex = Mutex.new
-
-  def self.synchronize(&block)
-    @mutex.synchronize(&block)
-  end
-
   def self.bytes(size)
     Sodium::Buffer.empty(size) do |buffer|
-      self.synchronize do
-        Sodium::FFI::Random.randombytes_buf(
-          buffer.to_str,
-          buffer.bytesize
-        )
-      end
+      Sodium::FFI::Random.randombytes_buf(
+        buffer.to_str,
+        buffer.bytesize
+      )
     end
   end
 
   def self.integer(max = 2 ** 32 - 1)
-    self.synchronize do
-      Sodium::FFI::Random.randombytes_uniform(max)
-    end
+    Sodium::FFI::Random.randombytes_uniform(max)
   end
 end
